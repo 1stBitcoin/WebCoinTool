4f0d11b2939624973eae6010c253a51127fea339
Zdenek Humpal <34726290+1stBitcoin@users.noreply.github.com>
2018-08-13 01:30:09 +0200

    Update call.php

--- cfg/call.php
+++ cfg/call.php
@@ -1,5 +1,4 @@
 <?php
 
-
-$bitcoin = new Bitcoin('tHebasisrpc','DuHgc7y3xDuLgF48AnLLgF48AnL3xraEdk7ofXch','192.168.1.53','1901');
-$bitcoin = new Bitcoin('tHebasisrpc','DuHgc7y3xDuLgF48AnLLgF48AnL3xraEdk7ofXch','192.168.1.51','1901');
+// This call connects to your coin deamon.
+$bitcoin = new Bitcoin('Username','PassWord','127.0.0.1','1901');
